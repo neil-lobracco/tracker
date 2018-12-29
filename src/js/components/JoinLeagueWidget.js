44f2d7d9d5c4f9f1f6fadbb6833d87ce3c0ae793
Neil LoBracco <neil.lobracco@gmail.com>
2018-12-29 15:02:25 -0500

    refactor joinleaguewidget

--- src/js/components/JoinLeagueWidget.js
+++ src/js/components/JoinLeagueWidget.js
@@ -3,29 +3,16 @@ import { connect } from 'react-redux';
 import { joinLeague } from '../actions';
 
 const mapDispatchToProps = dispatch => {
-  return {
-    joinLeague: (leagueId) => dispatch(joinLeague(leagueId)),
-  };
+  return { joinLeague: (leagueId) => dispatch(joinLeague(leagueId)) };
 };
+const mapStateToProps = (state) => ({ leagues: state.leagues, user: state.userContext.currentUser });
 
-const mapStateToProps = (state) => ({ leagues: state.leagues.all, leagueId: state.leagues.current,
-	 user: state.userContext.currentUser, currentLeagueMembership: state.leagues.currentMembership });
-
-class JoinLeagueWidget extends React.Component {
-	showWidget() {
-		return this.props.leagues != null && this.props.leagueId && this.props.user && !this.props.currentLeagueMembership;
-	}
-	joinLeague() {
-        this.props.joinLeague(this.props.leagueId);
-	}
-	render() {
-		if (this.showWidget()) {
-			return (
-				<div className='join-league' onClick={this.joinLeague.bind(this)}>
-					Join this league
-				</div>);
-		} else { return null; }
-	}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(JoinLeagueWidget);
+const JoinLeagueWidget = connect(mapStateToProps, mapDispatchToProps)(({joinLeague, leagues, user}) => {
+	if (leagues.all != null && leagues.current && user && !leagues.currentMembership) {
+		return (
+			<div className='join-league' onClick={() => joinLeague(leagues.current)}>
+				Join this league
+			</div>);
+	} else { return null; }
+});
+export default JoinLeagueWidget;

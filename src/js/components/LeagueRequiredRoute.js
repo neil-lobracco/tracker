51caca890212a443cb7125337e83af90d2ae26d7
Neil LoBracco <neil.lobracco@gmail.com>
2018-12-29 15:16:44 -0500

    last functional component refactors

--- src/js/components/LeagueRequiredRoute.js
+++ src/js/components/LeagueRequiredRoute.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-class LeagueRequiredRoute extends React.Component {
-  render() {
-    const { component: Component, ...rest } = this.props;
-    return (
-      <Route {...rest} component={connect(state => ({leagueId: state.leagues.current}))( props =>
-      {
-      return props.leagueId != null ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/leagues",
-            state: { from: props.location }
-          }} />) ;
-      }
-      )}/>);
-  }
-}
+const LeagueRequiredRoute = ({ component: Component, ...rest } ) => {
+  return (
+    <Route {...rest} component={connect(state => ({leagueId: state.leagues.current}))( props =>
+    {
+    return props.leagueId != null ? (
+      <Component {...props} />
+    ) : (
+      <Redirect
+        to={{
+          pathname: "/leagues",
+          state: { from: props.location }
+        }} />) ;
+    }
+  )}/>);
+};
 
 export default LeagueRequiredRoute;
--- src/js/components/PlayerList.js
+++ src/js/components/PlayerList.js
@@ -4,33 +4,31 @@ import { Link } from 'react-router-dom';
 const mapStateToProps = state => {
   return { players: state.players, user: state.userContext.currentUser };
 };
-class ConnectedList extends React.Component {
-  getFancyName(player, idx) {
-    let prefix = '';
-    if (idx == 0) {
-      prefix = '😁 ';
-    } else if (idx == (this.props.players.length - 1)) {
-      prefix = '😞 ';
-    }
-    if (this.props.user && this.props.user.id == player.id) {
-      prefix += '☝️';
-    }
-    return prefix + player.name;
+const getFancyName = ({player, idx, user, players}) => {
+  let prefix = '';
+  if (idx == 0) {
+    prefix = '😁 ';
+  } else if (idx == (players.length - 1)) {
+    prefix = '😞 ';
   }
-  render() {
-    return this.props.players == null ? <div>Loading...</div> : (
+  if (user && user.id == player.id) {
+    prefix += '☝️';
+  }
+  return prefix + player.name;
+};
+
+const PlayerList = connect(mapStateToProps)(({players, user}) => {
+  return players == null ? <div>Loading...</div> : (
     <table className="table">
     	<thead><tr><th>Player Name</th><th>Current Elo</th></tr></thead>
     	<tbody>
-      {this.props.players.map((player, idx) => (
-        <tr key={player.id || 'unknown'} className={player.games_played == 0 ? 'inactive' : ''}>
-        	<th>{player.id ? <Link to={`/players/${player.id}`}>{ this.getFancyName(player, idx) }</Link> : this.getFancyName(player.name, idx)}</th>
+      {players.map((player, idx) => (
+        <tr key={player.id} className={player.games_played == 0 ? 'inactive' : ''}>
+        	<th><Link to={`/players/${player.id}`}>{ getFancyName({player, idx, user, players}) }</Link></th>
         	<td>{Math.round(player.elo)}</td>
         </tr>
       ))}
       </tbody>
     </table>);
-}
-}
-const List = connect(mapStateToProps)(ConnectedList);
-export default List;
+});
+export default PlayerList;

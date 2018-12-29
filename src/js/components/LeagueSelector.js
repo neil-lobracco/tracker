a341642ba445405f9d3dee8337506ad98b773a0c
Neil LoBracco <neil.lobracco@gmail.com>
2018-12-29 13:25:17 -0500

    refactor league selection

--- src/js/components/LeagueSelector.js
+++ src/js/components/LeagueSelector.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { setLeague } from '../actions';
+
+const SelectableLeague = withRouter(
+	({location, history, league, current, setLeague}) => {
+		return(
+			<li key={league.id} className={`league ${league.id == current ? 'current':''}`}>
+				<span onClick={() => setLeague(league.id, current, location, history)}>{league.name}</span></li>
+		);
+	}
+);
 
 const mapDispatchToProps = dispatch => {
   return {
-    setLeague: (leagueId) => dispatch(setLeague(leagueId)),
+    setLeague: (leagueId, current, location, history) => {
+		if (leagueId == current) { return; }
+		dispatch(setLeague(leagueId));
+		const locState = location.state;
+		if (locState && locState.from) {
+			history.push(locState.from);
+		}
+	},
   };
 };
 
 const mapStateToProps = (state) => ({ leagues: state.leagues.all, leagueId: state.leagues.current });
 
-class LeagueSelector extends React.Component {
-	leaguesLoaded() {
-		return this.props.leagues != null;
-	}
-	selectLeague(leagueId) {
-		if (leagueId == this.props.leagueId) { return; }
-		this.props.setLeague(leagueId);
-		const locState = this.props.location.state;
-		if (locState && locState.from) {
-			this.props.history.push(locState.from);
-		}
-	}
-	render() {
-		if (this.leaguesLoaded()) {
-			return (
-				<div className='league-selector'>
-					<h2 className='subtitle'>Select a league</h2>
-					<br/>
-					<ul className='league-list'>
-						{this.props.leagues.map(league => {
-							return <li key={league.id} className={`league ${league.id == this.props.leagueId ? 'current':''}`}
-							><span onClick={this.selectLeague.bind(this, league.id)}>{league.name}</span></li>;
-						})}
-					</ul>
-				</div>);
-		} else {
-			return <div>Loading leagues...</div>;
-		}
-	}
-}
+const LeagueList = connect(mapStateToProps, mapDispatchToProps)(({ leagues, leagueId, setLeague}) => {
+	return (
+		<ul className='league-list'>
+			{(leagues || []).map(league => <SelectableLeague key={league.id} league={league} current={leagueId} setLeague={setLeague}/>)}
+		</ul>);
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(LeagueSelector);
+const LeagueSelector = () => (
+	<div className='league-selector'>
+		<h2 className='subtitle'>Select a league</h2>
+		<br/>
+		<LeagueList/>
+	</div>
+);
+export default LeagueSelector;
--- src/js/components/LeagueWidget.js
+++ src/js/components/LeagueWidget.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 
-class LeagueWidget extends React.Component {
-	leagueInfo() {
-		if (this.props.leagues != null) {
-			const league = this.props.leagues.find(l => l.id == this.props.leagueId);
-			if (league != null) {
-				return <span>Current League: {league.name}</span>;
-			}
+const CurrentLeague = connect(state => ({leagues: state.leagues}))(({leagues}) => {
+	if (leagues.all != null) {
+		const league = leagues.all.find(l => l.id == leagues.current);
+		if (league != null) {
+			return <span>Current League: {league.name}</span>;
 		}
 	}
-	render() {
-        return (
-            <div className="navbar-item has-dropdown is-hoverable">
-                <a className="navbar-link">
-                    {this.leagueInfo()}
-                </a>
-                <div className="navbar-dropdown">
-					<NavLink to={{ pathname: "/leagues", state: { from: window.location.pathname }}}
-     activeClassName='hidden'>Change League</NavLink>
-                </div>
-            </div>
-        );
-	}
-}
-export default connect(state => ({leagueId : state.leagues.current, leagues: state.leagues.all}))(LeagueWidget);
+	return null;
+});
+const ChangeLeagueLink = withRouter(({location}) => (
+	<NavLink to={{ pathname: "/leagues", state: { from: location.pathname }}}
+		activeClassName='hidden'>Change League
+	</NavLink>)
+);
+const LeagueWidget = () => {
+	return (
+		<div className="navbar-item has-dropdown is-hoverable">
+			<a className="navbar-link">
+				<CurrentLeague/>
+			</a>
+			<div className="navbar-dropdown">
+				<ChangeLeagueLink/>
+			</div>
+		</div>
+	);
+};
+export default LeagueWidget;
